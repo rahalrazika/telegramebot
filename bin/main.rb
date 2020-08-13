c6bbe02826802d9546b756538662abedf6233ca6
rahalrazika <rere055@gmail.com>
2020-08-14 00:26:25 +0200

    add main file

---
+++ bin/main.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require_relative '../lib/message.rb'
+require_relative '../lib/translater'
+
+puts '**************************************'
+puts '       Welcome  to Translator bot'
+puts '**************************************'
+
+puts '   I hope you will enjoy the bot '
+puts '   Use /start to start the bot '
+puts '   Use /stop to stop the bot '
+puts '   Use translate/(the word) to translate  '
+puts '   Use (ctr+c) on windows and (cmd+c) on Mac to stop the bot '
+
+Translatbot.new
